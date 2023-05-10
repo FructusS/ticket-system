c5ecb8f27670bb66201317cab7d987f12588efd9
Fructuss <Afanasev10056@gmail.com>
2023-05-10 20:16:47 +0300

    model for update or delete user

---
+++ TiketSystem/Shared/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketSystem.Shared.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string? UserPatronymic { get; set; }
+
+        public string? UserSurname { get; set; }
+        public string UserRole { get; set; }
+
+        public string? UserFirstName { get; set; }
+
+    }
+}
