de3a1670d2e8055039d7440cbaed3db4e5ffe81c
Fructuss <Afanasev10056@gmail.com>
2023-05-10 20:17:11 +0300

    request user model

---
+++ TiketSystem/Shared/Models/UserRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TicketSystem.Shared.ViewModels
+{
+    public class UserRequestModel
+    {
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
