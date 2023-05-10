629c7a2e393fa00f0986b8bbe227a25a34053620
Fructuss <Afanasev10056@gmail.com>
2023-05-10 20:13:13 +0300

    add  event aggregator for navigation

--- TiketSystem/Frontend/Desktop/viewmodels/MainViewModel.cs
+++ TiketSystem/Frontend/Desktop/viewmodels/MainViewModel.cs
@@ -1,43 +1,30 @@
-using System.Collections.ObjectModel;
-using System.Net.Http;
-using Microsoft.AspNetCore.SignalR.Client;
-using ViewModels;
-using MyTask = TicketSystem.Database.Models.Task;
-namespace TicketSystem.Desktop.viewmodels;
+using Prism.Events;
+using Shared.ViewModels;
+using TicketSystem.Desktop.Events;
+namespace TicketSystem.Desktop.ViewModels;
 
 public class MainViewModel : BaseViewModel
 {
-    public object CurrentPage { get; set; }
-
-
-    HttpClient client;
-    HttpClientHandler clientHandler;
-    HubConnection hubConnection;
-    private string title;
-    private ObservableCollection<MyTask> tasks = new();
-    public string Title
+    private object _currentPage;
+    public object CurrentPage
     {
-        get { return title; }
+        get { return _currentPage; }
         set
         {
-            title = value;
-            OnPropertyChanged("Title");
+            _currentPage = value;
+            OnPropertyChanged("CurrentPage");
         }
     }
 
-    public ObservableCollection<MyTask> Tasks
+    public MainViewModel(IEventAggregator eventAggregator)
     {
-        get { return tasks; }
-        set
-        {
-            tasks = value;
-            OnPropertyChanged("Tasks");
-        }
+        CurrentPage = new LoginViewModel(eventAggregator);
+
+        eventAggregator.GetEvent<LoginEvent>().Subscribe(OnLogin);
     }
 
-    public MainViewModel()
+    private void OnLogin(UserResponseModel userModel)
     {
-        CurrentPage = new TicketViewModel();
-
+        CurrentPage = new TicketViewModel(userModel);
     }
 }
