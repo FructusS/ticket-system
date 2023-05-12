db7b659c8e8d637db8dfeef0817fe0b68c68c1bd
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:02:02 +0300

    add map for task

--- TiketSystem/Backend/TicketSystem.Backend/Mapping/MappingProfile.cs
+++ TiketSystem/Backend/TicketSystem.Backend/Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Shared.ViewModels;
 using TicketSystem.Database.Models;
 using TicketSystem.Shared.ViewModels;
+using Task = TicketSystem.Database.Models.Task;
 
 namespace TicketSystem.Backend.Mapping
 {
@@ -9,12 +11,23 @@ namespace TicketSystem.Backend.Mapping
         public MappingProfile()
         {
             CreateMap<User, UserViewModel>()
-                .ForMember(x => x.UserName, opt => opt.MapFrom((x => x.UserName)))
-                .ForMember(x=> x.UserRole, opt => opt.MapFrom(x=> x.UserRole.Title));
+                .ForMember(x => x.UserName, opt => opt.MapFrom(x => x.UserName));
+              //  .ForMember(x => x.UserRole, opt => opt.MapFrom(x => x.UserRole.Title));
 
             CreateMap<UserViewModel, User>();
 
 
+            CreateMap<Task, TaskViewModel>()
+                .ForMember(x => x.TaskStatus, opt => opt.MapFrom(x => x.TaskStatus))
+                .ForMember(x => x.User, opt => opt.MapFrom(x => x.User))
+                .ForMember(x => x.Cabinet, opt => opt.MapFrom(x => x.Cabinet))
+                .ForMember(x => x.Description, opt => opt.MapFrom(x => x.Description));
+
+            CreateMap<TaskViewModel, Task>()
+                .ForMember(x => x.TaskStatus, opt => opt.MapFrom(x => x.TaskStatus))
+                .ForMember(x => x.User, opt => opt.MapFrom(x => x.User))
+                .ForMember(x => x.Cabinet, opt => opt.MapFrom(x => x.Cabinet))
+                .ForMember(x => x.Description, opt => opt.MapFrom(x => x.Description));
         }
     }
 }
