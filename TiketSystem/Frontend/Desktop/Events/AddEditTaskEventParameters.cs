dfc50ac5a61b944e398820c2e17e4461a8587c3e
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:12:11 +0300

    parameter for add edit task event

---
+++ TiketSystem/Frontend/Desktop/Events/AddEditTaskEventParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.Backend.Controllers;
+using Task = TicketSystem.Database.Models.Task;
+
+namespace TicketSystem.Desktop.Events
+{
+    public class AddEditTaskEventParameters
+    {
+        public Task Task { get; set; }
+        public LoginResponseViewModel LoginResponseViewModel { get; set; }
+    }
+}
