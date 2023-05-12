3594b5dce15fbe87f804e922e9c8ff40f36443e3
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:04:10 +0300

    change default value task status

--- TiketSystem/Backend/TicketSystem.Database/Models/Task.cs
+++ TiketSystem/Backend/TicketSystem.Database/Models/Task.cs
@@ -14,15 +14,16 @@ public partial class Task
     public DateTime? CompletedAt { get; set; }
 
     public string? Description { get; set; }
-    [DefaultValue(1)]
+    [DefaultValue(4)]
     public int? TaskStatusId { get; set; }
 
     public string? Title { get; set; }
 
     public string? Cabinet { get; set; }
 
-    public int? UserId { get; set; }
+    public string UserId { get; set; }
 
-    public virtual TaskStatus TaskStatus { get; set; } = null!;
-    public virtual User User { get; set; } = null!;
+    public virtual TaskStatus? TaskStatus { get; set; } = null!;
+    public virtual User? User { get; set; } = null!;
+
 }
