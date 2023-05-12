50f5898804d659b2e4d2d2767241dd973bea9c23
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:13:58 +0300

    add model for auth user

---
+++ TiketSystem/Shared/Models/UserModels/LoginResponseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketSystem.Backend.Controllers;
+
+public class LoginResponseViewModel
+{
+    public string Token { get; set; }
+    public string UserName { get; set; }
+    public string UserRole { get; set; }
+    public string UserId { get; set; }
+    public string? ConnectionId { get; set; }
+}
