546a18c81ee83b83a07f9e3075e57f3f056fa8a5
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:14:55 +0300

    add user role class

---
+++ TiketSystem/Shared/Models/UserModels/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ViewModels.UserModels
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Teacher = "Teacher";
+    }
+}
