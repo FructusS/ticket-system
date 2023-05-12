cf543f925bc636665631c1aa4d626c565b4ab46c
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:09:06 +0300

    add identity user

--- TiketSystem/Backend/TicketSystem.Database/Models/User.cs
+++ TiketSystem/Backend/TicketSystem.Database/Models/User.cs
@@ -1,31 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace TicketSystem.Database.Models;
 
-public partial class User
+public partial class User : IdentityUser
 {
-    public User()
-    {
-    }
-    public int UserId { get; set; }
-
-    public string? UserName { get; set; }
-
-    public string? UserPassword { get; set; }
-
     public string? UserPatronymic { get; set; }
 
     public string? UserSurname { get; set; }
 
     public string? UserFirstName { get; set; }
+    [JsonIgnore]
+    public override int AccessFailedCount { get; set; }
 
-    public int UserRoleId { get; set; }
+
 
     [JsonIgnore]
-    public virtual ICollection<Task> Tasks { get; } = new List<Task>();    
-    
+    public virtual ICollection<Task> Tasks { get; } = new List<Task>();
 
-    public virtual UserRole UserRole { get; set; }
 }
