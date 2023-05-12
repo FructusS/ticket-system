b323b35a5d4f9f4af0a07ff14bfa548c7e732483
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:13:46 +0300

    add model for register user

---
+++ TiketSystem/Shared/Models/UserModels/LoginRequestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ViewModels.UserModels
+{
+    public class LoginRequestViewModel
+    {
+        [Required(ErrorMessage = "Логин обязателен")]
+
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
