e0a4d42ce894769585b413d09e8ced7dfb42ee9d
Fructuss <Afanasev10056@gmail.com>
2023-05-12 20:01:29 +0300

    add edit add task subsctibe

--- TiketSystem/Frontend/Desktop/viewmodels/MainViewModel.cs
+++ TiketSystem/Frontend/Desktop/viewmodels/MainViewModel.cs
@@ -1,10 +1,14 @@
 using Prism.Events;
 using Shared.ViewModels;
+using Shared.ViewModels.UserModels;
+using TicketSystem.Backend.Controllers;
+using TicketSystem.Database.Models;
 using TicketSystem.Desktop.Events;
 namespace TicketSystem.Desktop.ViewModels;
 
 public class MainViewModel : BaseViewModel
 {
+    private readonly IEventAggregator _eventAggregator;
     private object _currentPage;
     public object CurrentPage
     {
@@ -12,19 +16,26 @@ public class MainViewModel : BaseViewModel
         set
         {
             _currentPage = value;
-            OnPropertyChanged("CurrentPage");
+            OnPropertyChanged();
         }
     }
 
     public MainViewModel(IEventAggregator eventAggregator)
     {
         CurrentPage = new LoginViewModel(eventAggregator);
-
+        _eventAggregator = eventAggregator; 
         eventAggregator.GetEvent<LoginEvent>().Subscribe(OnLogin);
+        eventAggregator.GetEvent<AddEditTaskEvent>().Subscribe(OnAddEditTask);
     }
 
-    private void OnLogin(UserResponseModel userModel)
+    private void OnAddEditTask(AddEditTaskEventParameters addEditTaskEventParameters)
     {
-        CurrentPage = new TicketViewModel(userModel);
+        CurrentPage = new AddEditTaskViewModel(addEditTaskEventParameters.Task, addEditTaskEventParameters.LoginResponseViewModel, _eventAggregator);
+    }
+
+    private void OnLogin(LoginResponseViewModel userModel)
+    {
+        CurrentPage = new TicketViewModel(userModel, _eventAggregator);
     }
 }
+
